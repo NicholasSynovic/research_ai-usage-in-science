124b3a1399c7941eae2bb976e4662f23a811fc14
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-04-06 15:17:32 -0500

    Add code to identify if DL is used in papers post author agreement

--- author-agreement/createAuthorAgreementResults.py
+++ author-agreement/createAuthorAgreementResults.py
@@ -4,8 +4,13 @@ from typing import Any, List
 
 import click
 import pandas
-from pandas import DataFrame, ExcelFile
+from pandas import DataFrame, ExcelFile, Series
 from pandas.core.groupby import DataFrameGroupBy
+
+
+def loadExcelFile(fp: Path, sheetName: str) -> DataFrame:
+    ef: ExcelFile = ExcelFile(path_or_buffer=fp, engine="openpyxl")
+    return pandas.read_excel(io=ef, sheet_name=sheetName)
 
 
 def createBaseDF(df: DataFrame) -> DataFrame:
@@ -26,19 +31,34 @@ def createBaseDF(df: DataFrame) -> DataFrame:
         if len(dlUsage) == 1:
             datum["uses_dl"] = True if dlUsage[0] == "Yes" else False
         else:
-            datum["uses_dl"] = False
+            datum["uses_dl"] = None
 
         data.append(datum)
 
     return DataFrame(data=data)
 
 
+def checkIfDLIsUsed(df: DataFrame, dlUsageDF: DataFrame) -> None:
+    """
+    Returns in-place modifications to `df`
+    """
+    idx: int
+    row: Series
+    for idx, row in df.iterrows():
+        if row["uses_dl"] is None:
+            doi: str = row["doi"]
+            if dlUsageDF[dlUsageDF["DOIs"] == doi]["Nick"].values[0] is True:
+                row["uses_dl"] = True
+            else:
+                row["uses_dl"] = False
+
+
 @click.command()
 @click.option(
-    "-i",
-    "--input",
-    "inputFP",
-    help="Path to input file",
+    "-f",
+    "--form-responses",
+    "formResponsesFP",
+    help="Path to form responses file",
     type=click.Path(
         exists=True,
         file_okay=True,
@@ -48,12 +68,34 @@ def createBaseDF(df: DataFrame) -> DataFrame:
     ),
     required=True,
 )
-def main(inputFP: Path) -> None:
-    excelFile: ExcelFile = ExcelFile(path_or_buffer=inputFP, engine="openpyxl")
-    excelDF: DataFrame = pandas.read_excel(io=excelFile, sheet_name="Form1")
+@click.option(
+    "-0",
+    "--dl-usage",
+    "dlUsageFP",
+    help="Path to author agreement file for 'Do the author's use DL?'",
+    type=click.Path(
+        exists=True,
+        file_okay=True,
+        readable=True,
+        resolve_path=True,
+        path_type=Path,
+    ),
+    required=True,
+)
+def main(formResponsesFP: Path, dlUsageFP: Path) -> None:
+    currentExcelDF: DataFrame = loadExcelFile(
+        fp=formResponsesFP,
+        sheetName="Form1",
+    )
+    df: DataFrame = createBaseDF(df=currentExcelDF)
 
-    df: DataFrame = createBaseDF(df=excelDF)
-    print(df)
+    currentExcelDF = loadExcelFile(
+        fp=dlUsageFP,
+        sheetName="Sheet1",
+    )
+    checkIfDLIsUsed(df=df, dlUsageDF=currentExcelDF)
+
+    df.to_csv(path_or_buf="output.csv", index=False)
 
 
 if __name__ == "__main__":
