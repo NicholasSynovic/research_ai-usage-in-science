7488c3d2a1264e7fc41b7a506e8016c08ae56fcc
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-04-23 11:32:04 -0500

    Leverage optparse in run.bash

--- run.bash
+++ run.bash
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-AIUS_DB="./aius.db"
-AUTHOR_AGREEMENT_WORKBOOK="./author_agreement.xlsx"
+#!/bin/bash
+
+source optparse.bash
+optparse.define short=d long=database desc="The database to output and read data from" variable=AIUS_DB default=aius.db
+optparse.define short=a long=author-agreement desc="The author agreement Excel workbook file" variable=AUTHOR_AGREEMENT_WORKBOOK default=author_agreement.xlsx
+optparse.define short=e long=email desc="Email to access OpenAlex polite pool" variable=EMAIL
+source $( optparse.build )
 
 ./aius init --db $AIUS_DB
 ./aius search --db $AIUS_DB --journal nature
 ./aius search --db $AIUS_DB --journal plos
 ./aius search --db $AIUS_DB --journal science
 ./aius extract-documents --db $AIUS_DB
-./aius openalex --db $AIUS_DB --email $1
+./aius openalex --db $AIUS_DB --email $EMAIL
 ./aius filter --db $AIUS_DB
 ./aius author-agreement --db $AIUS_DB --workbook $AUTHOR_AGREEMENT_WORKBOOK
