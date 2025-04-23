4ad88747933f6cc90d7c7a7b8947caac61d28875
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-04-23 11:34:54 -0500

    Fix path to file

--- run.bash
+++ run.bash
@@ -8,11 +8,11 @@ optparse.define short=a long=author-agreement desc="The author agreement Excel w
 optparse.define short=e long=email desc="Email to access OpenAlex polite pool" variable=EMAIL
 source $( optparse.build )
 
-./aius init --db $AIUS_DB
-./aius search --db $AIUS_DB --journal nature
-./aius search --db $AIUS_DB --journal plos
-./aius search --db $AIUS_DB --journal science
-./aius extract-documents --db $AIUS_DB
-./aius openalex --db $AIUS_DB --email $EMAIL
-./aius filter --db $AIUS_DB
-./aius author-agreement --db $AIUS_DB --workbook $AUTHOR_AGREEMENT_WORKBOOK
+./dist/aius init --db $AIUS_DB
+./dist/aius search --db $AIUS_DB --journal nature
+./dist/aius search --db $AIUS_DB --journal plos
+./dist/aius search --db $AIUS_DB --journal science
+./dist/aius extract-documents --db $AIUS_DB
+./dist/aius openalex --db $AIUS_DB --email $EMAIL
+./dist/aius filter --db $AIUS_DB
+./dist/aius author-agreement --db $AIUS_DB --workbook $AUTHOR_AGREEMENT_WORKBOOK
