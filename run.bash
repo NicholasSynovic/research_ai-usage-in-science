2fd430ae36239b194d5c6dca977bb5c1602dd09b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-04-23 13:26:36 -0500

    Remvoe comments

--- run.bash
+++ run.bash
@@ -8,11 +8,11 @@ optparse.define short=a long=author-agreement desc="The author agreement Excel w
 optparse.define short=e long=email desc="Email to access OpenAlex polite pool" variable=EMAIL
 source $( optparse.build )
 
-# ./dist/aius init --db $AIUS_DB
-# ./dist/aius search --db $AIUS_DB --journal nature
-# ./dist/aius search --db $AIUS_DB --journal plos
-# ./dist/aius search --db $AIUS_DB --journal science
-# ./dist/aius extract-documents --db $AIUS_DB
-# ./dist/aius openalex --db $AIUS_DB --email $EMAIL
-# ./dist/aius filter --db $AIUS_DB
+./dist/aius init --db $AIUS_DB
+./dist/aius search --db $AIUS_DB --journal nature
+./dist/aius search --db $AIUS_DB --journal plos
+./dist/aius search --db $AIUS_DB --journal science
+./dist/aius extract-documents --db $AIUS_DB
+./dist/aius openalex --db $AIUS_DB --email $EMAIL
+./dist/aius filter --db $AIUS_DB
 ./dist/aius author-agreement --db $AIUS_DB --workbook $AUTHOR_AGREEMENT_WORKBOOK
