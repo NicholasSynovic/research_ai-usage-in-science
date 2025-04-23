f73297088e864a494b299ca27f4a92b78be05d9f
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-04-23 10:40:35 -0500

    Replace ls with find

--- scripts/automatedAnalysis/runMain.bash
+++ scripts/automatedAnalysis/runMain.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-ls *.pdf | xargs -I % python main.py -i % -m "gemma3:27b" -p "Do the author's use deep learning (including artificial neural networks) methods in their paper?" -t 120 --prediction-tokens 10 --context-tokens 64000 >> usesDL.txt
-ls *.pdf | xargs -I % python main.py -i % -m "gemma3:27b" -p "Do the author's use pre-trained deep learning models in their paper?" -t 120 --prediction-tokens 10 --context-tokens 64000 >> usesPTMs.txt
+# find bulk_pdfs -name "*.pdf" -exec echo "Hello {}" \;
+
+find author-agreement-docs -name "*.pdf" -exec python automatedAnalysis.py -i {} -m "qwq" -p "Do the author's use deep learning methods in their paper?" -t 600 --prediction-tokens 2000 --context-tokens 38000 >> usesDL.txt \;
+
+# ls author-agreement-docs/*.pdf | xargs -I % python automatedAnalysis.py -i % -m "qwq" -p "Do the author's use pre-trained deep learning models in their paper?" -t 600 --prediction-tokens 2000 --context-tokens 38000 >> usesPTMs.txt
+# ls author-agreement-docs/*.pdf | xargs -I % python automatedAnalysis.py -i % -m "qwq" -p "What pre-trained deep learning models in their paper?" -s "Respond as a list of deep learning model names" -t 600 --prediction-tokens 4000 --context-tokens 36000 >> ptmsUsed.txt
+# ls author-agreement-docs/*.pdf | xargs -I % python automatedAnalysis.py -i % -m "qwq" -p "How are pre-trained models reused?" -s "Respond as succinctly as possible" -t 600 --prediction-tokens 4000 --context-tokens 36000 >> ptmReuse.txt
