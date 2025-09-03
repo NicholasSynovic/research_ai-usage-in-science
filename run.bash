395a3d6dd1a471c0acdbe5e634797e151967b6a6
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-09-03 15:38:48 -0500

    Delete old files

--- run.bash
+++ run.bash
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-#!/bin/bash
-
-source optparse.bash
-optparse.define short=d long=database desc="The database to output and read data from" variable=AIUS_DB default=aius.db
-optparse.define short=a long=author-agreement desc="The author agreement Excel workbook file" variable=AUTHOR_AGREEMENT_WORKBOOK default=author_agreement.xlsx
-optparse.define short=e long=email desc="Email to access OpenAlex polite pool" variable=EMAIL
-source $( optparse.build )
-
-./dist/aius init --db $AIUS_DB
-./dist/aius search --db $AIUS_DB --journal nature
-./dist/aius search --db $AIUS_DB --journal plos
-./dist/aius search --db $AIUS_DB --journal science
-./dist/aius extract-documents --db $AIUS_DB
-./dist/aius openalex --db $AIUS_DB --email $EMAIL
-./dist/aius filter --db $AIUS_DB
-./dist/aius author-agreement --db $AIUS_DB --workbook $AUTHOR_AGREEMENT_WORKBOOK
+aius search --journal plos
+aius search --journal nature
+aius extract-documents
+aius openalex --email nsynovic@luc.edu
+aius filter
