f0037f4b102b841ab76cd7a6f9d4cd35dce17eba
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-09-15 13:46:11 -0500

    Get stats on Natural Science fields

--- openalex-topics/statistics.py
+++ openalex-topics/statistics.py
@@ -5,6 +5,8 @@ import pandas
 from pandas import DataFrame
 from progress.bar import Bar
 from requests import Response, get
+from aius import FIELD_FILTER
+from pandas.core.groupby import DataFrameGroupBy
 
 OA_TOPICS_API: str = "https://api.openalex.org/topics"
 PICKLE_PATH: Path = Path("oa_topics.pickle").absolute()
@@ -29,17 +31,46 @@ def get_all_pages(total_count: int = 4600, per_page: int = 100) -> DataFrame:
 
 def main() -> None:
     # Load data if it exists else get the data
-    df: DataFrame
+    topic_df: DataFrame
     if PICKLE_PATH.exists():
-        df = pickle.load(file=PICKLE_PATH.open(mode="rb"))
+        topic_df = pickle.load(file=PICKLE_PATH.open(mode="rb"))
     else:
-        df = get_all_pages()
-        PICKLE_PATH.write_bytes(data=pickle.dumps(obj=df))
+        topic_df = get_all_pages()
+        PICKLE_PATH.write_bytes(data=pickle.dumps(obj=topic_df))
 
-    print("Total topics:", df["id"].unique().size)
-    print("Total subfields:", DataFrame(df["subfield"].to_list())["id"].unique().size)
-    print("Total fields:", DataFrame(df["field"].to_list())["id"].unique().size)
-    print("Total domains:", DataFrame(df["domain"].to_list())["id"].unique().size)
+    # Convert dict into DataFrames
+    subfield_df: DataFrame = DataFrame(topic_df["subfield"].to_list())
+    field_df: DataFrame = DataFrame(topic_df["field"].to_list())
+    domain_df: DataFrame = DataFrame(topic_df["domain"].to_list())
+
+    # Output counts of each category
+    print("Total topics:", topic_df["id"].unique().size)
+    print("Total subfields:", subfield_df["id"].unique().size)
+    print("Total fields:", field_df["id"].unique().size)
+    print("Total domains:", domain_df["id"].unique().size)
+
+    # Create DataFrame where it is just the names of the attributes + topic works count
+    topic_df["subfield_name"] = topic_df["subfield"].apply(lambda x: x["display_name"])
+    topic_df["field_name"] = topic_df["field"].apply(lambda x: x["display_name"])
+    topic_df["domain_name"] = topic_df["domain"].apply(lambda x: x["display_name"])
+    df: DataFrame = topic_df[["display_name", "works_count", "subfield_name", "field_name", "domain_name",]]
+
+    # Filter for only Natural Science topics
+    ns_df = df[df["field_name"].isin(FIELD_FILTER)]
+
+    # Count the number of Natural Science topics, subfields, fields, and domains
+    print("Natural Science topics:", ns_df["display_name"].unique().size)
+    print("Natural Science subfields:", ns_df["subfield_name"].unique().size)
+    print("Natural Science fields:", ns_df["field_name"].unique().size)
+    print("Natural Science domains:", ns_df["domain_name"].unique().size)
+
+    # Count the number of topics and subfields per field
+    ns_dfgb: DataFrameGroupBy = ns_df.groupby(by="field_name")
+
+    _df: DataFrame
+    idx: str
+    for idx, _df in ns_dfgb:
+        print(idx, "topics", _df["display_name"].unique().size, "subfields", _df["subfield_name"].unique().size,)
 
 
 if __name__ == "__main__":
