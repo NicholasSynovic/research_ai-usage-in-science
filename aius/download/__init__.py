74e04a29e270f109ab4ad6f19947719e16ccc204
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-09-17 10:33:50 -0500

    Refactor method definitions

--- aius/download/__init__.py
+++ aius/download/__init__.py
@@ -39,7 +39,16 @@ class Downloader(ABC):
             )
         )
 
-    def extract_html_content(self) -> None:
+    def get_documents(
+        self,
+        column: str,
+        timeout: int = 60,
+    ) -> Generator[Response, None, None]:
+        row: Series
+        for _, row in self.paper_dois.iterrows():
+            yield get(url=row[column], timeout=timeout)
+
+    def download_html_content(self) -> None:
         data: list[str] = []
 
         resp: Response
@@ -53,102 +62,37 @@ class Downloader(ABC):
                 data.append(soup.prettify())
                 bar.next()
 
-        self.paper_dois["raw_html_content"] = data
+        self.paper_dois["html_raw"] = data
 
-    def extract_jats_content(self) -> None:
+    def download_jats_content(self) -> None:
         data: list[str] = []
 
         resp: Response
         with Bar("Downloading JATS XML content...", max=self.paper_count) as bar:
-            for resp in self.get_documents(column="html_url"):
+            for resp in self.get_documents(column="jats_url"):
                 data.append(resp.content.decode())
                 bar.next()
 
-        self.paper_dois["raw_jats_content"] = data
+        self.paper_dois["jats_raw"] = data
 
-    def extract_pdf_content(self) -> None:
+    def download_pdf_content(self) -> None:
         data: list[bytes] = []
 
         resp: Response
         with Bar("Downloading PDF content...", max=self.paper_count) as bar:
-            for resp in self.get_documents(column="html_url"):
+            for resp in self.get_documents(column="pdf_url"):
                 data.append(resp.content)
                 bar.next()
 
-        self.paper_dois["raw_pdf_content"] = data
-
-    def get_documents(
-        self,
-        column: str,
-        timeout: int = 60,
-    ) -> Generator[Response, None, None]:
-        row: Series
-        for _, row in self.paper_dois.iterrows():
-            yield get(url=row[column], timeout=timeout)
+        self.paper_dois["pdf_raw"] = data
 
 
-def download(df: DataFrame, journal_downloader: Downloader) -> DataFrame:
+def download(journal_downloader: Downloader) -> DataFrame:
     # Create URLs
     journal_downloader.create_html_urls()
     journal_downloader.create_jats_urls()
     journal_downloader.create_pdf_urls()
 
-    # Download
+    # Download content
 
     return journal_downloader.paper_dois
-
-
-# class Downloader:
-#     def __init__(self, data: DataFrame, pdf_dir: Path) -> None:
-#         self.data: DataFrame = data
-#         self.pdf_dir: Path = pdf_dir
-
-#     def _get_plos(self, doi: str) -> Response:
-#         # Get DOI prefix/suffix from DOI
-#         doi_prefix_suffix: str = doi.replace("https://doi.org/", "")
-
-#         # Create PLOS PDF URL
-#         url: str = f"https://journals.plos.org/plosone/article/file?id={doi_prefix_suffix}&type=printable"
-
-#         # Return the response
-#         return get(url=url, timeout=aius.GET_TIMEOUT)
-
-#     def _get_nature(self, doi: str) -> Response:
-#         # Get DOI suffix from DOI
-#         doi_suffix: str = doi.split(sep="/")[-1]
-
-#         # Create Nature PDF URL
-#         url: str = f"https://www.nature.com/articles/{doi_suffix}.pdf"
-
-#         # Return the response
-#         return get(url=url, timeout=aius.GET_TIMEOUT)
-
-#     def download(self) -> None:
-#         with Bar("Downloading data...", max=self.data.shape[0]) as bar:
-#             row: Series
-#             for _, row in self.data.iterrows():
-#                 # Create filename from doi prefix and suffix
-#                 filename: str = f"{row['doi'].replace('https://doi.org/', '').replace('/', '_')}.pdf"
-
-#                 # Create filepath for PDF
-#                 filepath: Path = Path(self.pdf_dir, filename)
-
-#                 # Check if file exists at that filepath and skip processing it
-#                 if filepath.exists():
-#                     bar.next()
-#                     continue
-
-#                 # Get PDF response
-#                 resp: Response
-#                 match row["journal"]:
-#                     case "nature":
-#                         resp = self._get_nature(doi=row["doi"])
-#                     case "plos":
-#                         resp = self._get_plos(doi=row["doi"])
-#                     case _:
-#                         raise TypeError("Not a valid journal")
-
-#                 # Write data from the response
-#                 filepath.write_bytes(data=resp.content)
-
-#                 bar.next()
