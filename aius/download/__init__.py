e766f85854d732f63a7d1655eda33f6a5d53a459
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-09-17 10:30:40 -0500

    Simplify PLOS downloader implementation

--- aius/download/__init__.py
+++ aius/download/__init__.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
 from collections.abc import Generator
-from typing import Literal
+from string import Template
 
+from bs4 import BeautifulSoup
 from pandas import DataFrame, Series
+from progress.bar import Bar
 from requests import Response, get
 
 
@@ -11,41 +13,69 @@ class Downloader(ABC):
         self.paper_dois = paper_dois
         self.paper_count: int = self.paper_dois.shape[0]
 
-    @abstractmethod
+        # Override these in implmentation classes
+        self.html_url_template: Template = Template("")
+        self.jats_url_template: Template = Template("")
+        self.pdf_url_template: Template = Template("")
+
     def create_html_urls(self) -> None:
-        """
-        Modifies self.paper_dois in place to add a new column `html_urls`.
+        self.paper_dois["html_url"] = self.paper_dois["doi"].apply(
+            lambda x: self.jats_url_template.substitute(
+                doi_prefix_suffix=x.replace("https://doi.org/", ""),
+            )
+        )
 
-        This column contains links to the PDF content of paper DOIs.
-        """
-        ...
-
-    @abstractmethod
     def create_jats_urls(self) -> None:
-        """
-        Modifies self.paper_dois in place to add a new column `jats_urls`.
+        self.paper_dois["jats_url"] = self.paper_dois["doi"].apply(
+            lambda x: self.jats_url_template.substitute(
+                doi_prefix_suffix=x.replace("https://doi.org/", ""),
+            )
+        )
 
-        This column contains links to the JATS XML content of paper DOIs.
-        """
-        ...
-
-    @abstractmethod
     def create_pdf_urls(self) -> None:
-        """
-        Modifies self.paper_dois in place to add a new column `pdf_urls`.
+        self.paper_dois["pdf_url"] = self.paper_dois["doi"].apply(
+            lambda x: self.pdf_url_template.substitute(
+                doi_prefix_suffix=x.replace("https://doi.org/", ""),
+            )
+        )
 
-        This column contains links to the PDF content of paper DOIs.
-        """
-        ...
+    def extract_html_content(self) -> None:
+        data: list[str] = []
 
-    @abstractmethod
-    def extract_html_content(self) -> None: ...
+        resp: Response
+        with Bar("Downloading HTML content...", max=self.paper_count) as bar:
+            for resp in self.get_documents(column="html_url"):
+                soup: BeautifulSoup = BeautifulSoup(
+                    markup=resp.content,
+                    features="lxml",
+                )
 
-    @abstractmethod
-    def extract_jats_content(self) -> None: ...
+                data.append(soup.prettify())
+                bar.next()
 
-    @abstractmethod
-    def extract_pdf_content(self) -> None: ...
+        self.paper_dois["raw_html_content"] = data
+
+    def extract_jats_content(self) -> None:
+        data: list[str] = []
+
+        resp: Response
+        with Bar("Downloading JATS XML content...", max=self.paper_count) as bar:
+            for resp in self.get_documents(column="html_url"):
+                data.append(resp.content.decode())
+                bar.next()
+
+        self.paper_dois["raw_jats_content"] = data
+
+    def extract_pdf_content(self) -> None:
+        data: list[bytes] = []
+
+        resp: Response
+        with Bar("Downloading PDF content...", max=self.paper_count) as bar:
+            for resp in self.get_documents(column="html_url"):
+                data.append(resp.content)
+                bar.next()
+
+        self.paper_dois["raw_pdf_content"] = data
 
     def get_documents(
         self,
