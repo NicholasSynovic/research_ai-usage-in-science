ee0df69896bf6a6842aca18567cf219887ae81fc
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-09-29 09:54:08 -0500

    Get the OpenAlex field works

--- figures/fig_a/get_data.py
+++ figures/fig_a/get_data.py
@@ -6,8 +6,11 @@ from requests import Response, get
 
 from aius import FIELD_FILTER
 
-URL_TEMPLATE: Template = Template(
+OA_TOPIC_URL_TEMPLATE: Template = Template(
     template="https://api.openalex.org/topics?per_page=100&page=${page}"
+)
+OA_FIELD_URL_TEMPLATE: Template = Template(
+    template="https://api.openalex.org/fields/${field_number}"
 )
 PAGES: int = ceil(4516 / 100)
 GOOD_STATUS_CODE: int = 200
@@ -20,11 +23,20 @@ OA_FIELD_NUMBERS: dict[str, int] = {
     "Neuroscience": 28,
     "Immunology and Microbiology": 24,
 }
+OA_FIELD_WORKS: dict[str, int] = {
+    "Agricultural and Biological Sciences": 10328653,
+    "Environmental Science": 9191866,
+    "Earth and Planetary Sciences": 4290599,
+    "Physics and Astronomy": 7186862,
+    "Chemistry": 5206967,
+    "Neuroscience": 3086383,
+    "Immunology and Microbiology": 2108297,
+}
 
 
 def _get_topic_results(page: int) -> list[dict]:
     # Using the OpenAlex topic API, get the JSON data
-    url: str = URL_TEMPLATE.substitute(page=page)
+    url: str = OA_TOPIC_URL_TEMPLATE.substitute(page=page)
     resp: Response = get(url=url, timeout=60)
 
     if resp.status_code != GOOD_STATUS_CODE:
@@ -65,3 +77,28 @@ def identify_topic_field_number() -> dict[str, int]:
             bar.next()
 
     return data
+
+
+def count_works_per_field() -> dict[str, int]:
+    data: dict[str, int] = {}
+
+    with Bar("Getting the works per OpenAlex field...", max=7) as bar:
+        key: str
+        key_number: int
+        for key, key_number in OA_FIELD_NUMBERS.items():
+            url: str = OA_FIELD_URL_TEMPLATE.substitute(field_number=key_number)
+            resp: Response = get(url=url, timeout=60)
+
+            if resp.status_code != GOOD_STATUS_CODE:
+                raise ValueError(f"Invalid response: {url} {resp.status_code}")
+
+            json: dict = resp.json()
+
+            data[key] = json["works_count"]
+
+            bar.next()
+
+    return data
+
+
+count_works_per_field()
