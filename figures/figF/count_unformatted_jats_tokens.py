84bf311e49b27519e9f40120bcf87f9d6062e263
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-09-29 21:29:25 -0500

    Refactor scripts

--- figures/figF/count_unformatted_jats_tokens.py
+++ figures/figF/count_unformatted_jats_tokens.py
@@ -1,47 +1,102 @@
-import pickle
-import zipfile
 from pathlib import Path
+from zipfile import ZipFile
 
+import click
 import pandas
 import tiktoken
+from bs4 import BeautifulSoup
+from bs4.element import Tag
 from pandas import DataFrame
 from progress.bar import Bar
+from tiktoken.core import Encoding
 
 from aius.db import DB
 
-sql_query: str = """
+# Query to get all natural science papers
+SQL_QUERY: str = """
 SELECT a.*
 FROM papers a
 JOIN ns_papers b ON a._id = b.paper_id;
 """
 
-data: dict[str, list] = {"filename": [], "tokens": [], "content": []}
 
-all_of_plos_archive: Path = Path("../data/all_of_plos.zip").resolve()
-db: DB = DB(db_path=Path("../AIUS.sqlite3").resolve())
+def read_file_from_archive(filename: str, archive: ZipFile) -> BeautifulSoup:
+    # Read file contents from zipfile
+    return BeautifulSoup(
+        markup=archive.open(name=filename, mode="r"),
+        features="lxml-xml",
+    )
 
-encoding = tiktoken.encoding_for_model("gpt-4")
-
-df: DataFrame = pandas.read_sql_query(sql=sql_query, con=db.engine)
-df = df[df["doi"].str.contains(pat="doi.org/10.1371")]
-df["filename"] = df["doi"].str.replace(pat="https://doi.org/10.1371/", repl="") + ".xml"
+def count_tokens(text: str, encoding: Encoding) -> int:
+    return len(encoding.encode(text=text))
 
 
-with Bar("Computing tokens...", max=df["filename"].size) as bar:
-    with zipfile.ZipFile(all_of_plos_archive, "r") as z:
-        for file in df["filename"]:
-            data["filename"].append(file)
+@click.command()
+@click.option(
+    "-i",
+    "--input-path",
+    help="Path to all PLOS documents in JATS XML format in a single zip file",
+    type=lambda x: Path(x).resolve(),
+)
+@click.option(
+    "-d",
+    "--db-path",
+    help="Path to AIUS SQLite3 database file",
+    type=lambda x: Path(x).resolve(),
+)
+@click.option(
+    "-o",
+    "--output-path",
+    help="Path to write output Apache Parquet file",
+    type=lambda x: Path(x).resolve(),
+)
+def main(input_path: Path, db_path: Path, output_path: Path) -> None:
+    # Data structure to hold content information
+    data: dict[str, list] = {"filename": [], "tokens": [], "content": []}
 
-            with z.open(file) as f:
-                text: str = (
-                    f.read().decode("utf-8").replace("\n", "")
-                )  # convert bytes → string
-                data["tokens"].append(len(encoding.encode(text=text)))
-                data["content"].append(text)
-                f.close()
+    # Connect to the database
+    db: DB = DB(db_path=db_path)
 
-            bar.next()
-        z.close()
+    # Instantiate encoding
+    encoding: Encoding = tiktoken.encoding_for_model("gpt-4")
 
-with open(file="unformatted_jats_tokens.pickle", mode="wb") as jt:
-    pickle.dump(obj=data, file=jt)
+    # Read data from the database into a DataFrame
+    df: DataFrame = pandas.read_sql_query(sql=SQL_QUERY, con=db.engine)
+
+    # Get only PLOS documents
+    df = df[df["doi"].str.contains(pat="doi.org/10.1371")]
+
+    # Drop DOI URL frontmatter
+    df["filename"] = (
+        df["doi"].str.replace(pat="https://doi.org/10.1371/", repl="") + ".xml"
+    )
+
+    # Extract text, compute token size, and store data
+    with Bar("Computing tokens...", max=df["filename"].size) as bar:
+        with ZipFile(input_path, "r") as archive:
+            filename: str
+            for filename in df["filename"]:
+                soup: BeautifulSoup = read_file_from_archive(
+                    filename=filename,
+                    archive=archive,
+                )
+                xml_string: str = soup.prettify()
+                token_count: int = count_tokens(
+                    text=xml_string,
+                    encoding=encoding,
+                )
+
+                data["filename"].append(filename)
+                data["content"].append(xml_string)
+                data["tokens"].append(token_count)
+
+                bar.next()
+
+            archive.close()
+
+    output_df: DataFrame = DataFrame(data=data)
+    output_df.to_parquet(path=output_path, engine="pyarrow")
+
+
+if __name__ == "__main__":
+    main()
