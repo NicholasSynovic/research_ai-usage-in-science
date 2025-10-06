c03c8d8d817bfac72d2ab7647f6177d0f3c5ace2
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-10-06 11:03:07 -0500

    Delete extra files

--- run.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# aius search --journal plos
-# aius search --journal nature
-# aius extract-documents
-# aius openalex --email nsynovic@luc.edu
-# aius filter
-aius download --directory data
