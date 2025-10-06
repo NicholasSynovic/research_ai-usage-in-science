6219fed5ca8cef115a81f90b9ee8b97c0fc4328e
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-10-06 11:21:41 -0500

    Move scripts to a different directory

---
+++ scripts/1-download-plos/download_plos.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget \
+  --no-clobber \
+  -O ../../data/all_of_plos.zip  \
+  https://allof.plos.org/allofplos.zip
--- scripts/download_allofplos.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-wget --no-clobber \
-    -O ../data/all_of_plos.zip  \
-    https://allof.plos.org/allofplos.zip
