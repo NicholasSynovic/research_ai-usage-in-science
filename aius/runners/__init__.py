2b51ae31420bf2b9af21a6a205a55daf2fedac66
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-11-11 17:24:11 -0600

    Add code to run the init code

--- aius/runners/__init__.py
+++ aius/runners/__init__.py
@@ -1,5 +1,13 @@
 from logging import Logger
 
+from aius.runners.init import InitRunner
+
 
 def init(logger: Logger, **kwargs) -> None:
-    logger.debug(msg=f"kwargs: {kwargs}")
+    logger.debug(msg=f"init kwargs: {kwargs}")
+    runner: InitRunner = InitRunner(
+        db_path=kwargs["init.db"],
+        min_year=kwargs["init.min_year"],
+        max_year=kwargs["init.max_year"],
+    )
+    runner.execute(logger=logger)
