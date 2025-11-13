ead91159ffc51a4e1ebcc0674f10068f538a7f0b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-11-13 14:03:16 -0600

    Format code

--- aius/runners/openalex.py
+++ aius/runners/openalex.py
@@ -1,15 +1,18 @@
-from logging import Logger
-from aius.db import DB
-from requests import Session, Response
-from requests.adapters import HTTPAdapter, Retry
-from string import Template
-import pandas
-from pydantic import BaseModel
-from pandas import DataFrame
-from json import dumps
-from progress.bar import Bar
-from aius.runners.runner import Runner
 from datetime import datetime, timezone
+from json import dumps
+from logging import Logger
+from string import Template
+
+import pandas
+from pandas import DataFrame
+from progress.bar import Bar
+from pydantic import BaseModel
+from requests import Response, Session
+from requests.adapters import HTTPAdapter, Retry
+
+from aius.db import DB
+from aius.runners.runner import Runner
+
 
 class MetadataModel(BaseModel):
     timestamp: float
@@ -21,7 +24,8 @@ class MetadataModel(BaseModel):
     topic_2: str | None
     json_data: dict
 
-def metadata_model_to_df(mm: MetadataModel) ->  DataFrame:
+
+def metadata_model_to_df(mm: MetadataModel) -> DataFrame:
     data: dict[str, list] = {
         "timestamp": [mm.timestamp],
         "doi": [mm.doi],
@@ -35,13 +39,16 @@ def metadata_model_to_df(mm: MetadataModel) ->  DataFrame:
 
     return DataFrame(data=data)
 
+
 class OpenAlexRunner(Runner):
     def __init__(self, logger: Logger, email: str, db: DB) -> None:
         # Set class constants
         self.logger: Logger = logger
         self.email: str = email
         self.db: DB = db
-        self.search_template: Template = Template(template="https://api.openalex.org/works?per-page=100&mailto=${email}&filter=doi:${dois}")
+        self.search_template: Template = Template(
+            template="https://api.openalex.org/works?per-page=100&mailto=${email}&filter=doi:${dois}"
+        )
 
         # Custom HTTPS session with exponential backoff enabled
         self.session: Session = Session()
@@ -53,8 +60,15 @@ class OpenAlexRunner(Runner):
         )
 
         # Get all DOIs from the database
-        dois: list[str] = ["https://doi.org/" + doi for doi in pandas.read_sql_query(sql="SELECT DISTINCT doi FROM articles;", con=self.db.engine)["doi"].tolist()]
-        self.doi_chunks: list[list[str]] = [dois[i:i + 50] for i in range(0, len(dois), 50)]
+        dois: list[str] = [
+            "https://doi.org/" + doi
+            for doi in pandas.read_sql_query(
+                sql="SELECT DISTINCT doi FROM articles;", con=self.db.engine
+            )["doi"].tolist()
+        ]
+        self.doi_chunks: list[list[str]] = [
+            dois[i : i + 50] for i in range(0, len(dois), 50)
+        ]
 
     def _write_data_to_table(self, data: DataFrame) -> None:
         self.logger.info(msg=f"Writing data to the `openalex` table")
@@ -84,13 +98,18 @@ class OpenAlexRunner(Runner):
 
         return (topic_0, topic_1, topic_2)
 
-    def search(self)    -> list[MetadataModel]:
+    def search(self) -> list[MetadataModel]:
         data: list[MetadataModel] = []
 
-        with Bar("Searching OpenAlex for DOI metadata...", max=len(self.doi_chunks)) as bar:
+        with Bar(
+            "Searching OpenAlex for DOI metadata...", max=len(self.doi_chunks)
+        ) as bar:
             chunk: list[str]
             for chunk in self.doi_chunks:
-                url: str = self.search_template.substitute(email=self.email, dois="|".join(chunk),)
+                url: str = self.search_template.substitute(
+                    email=self.email,
+                    dois="|".join(chunk),
+                )
                 self.logger.info(msg=f"Querying {url}")
 
                 timestamp: float = datetime.now(tz=timezone.utc).timestamp()
@@ -99,36 +118,37 @@ class OpenAlexRunner(Runner):
 
                 result: dict
                 for result in resp.json()["results"]:
-                    topic_0, topic_1, topic_2 = self.extract_topics(topics=result["topics"])
+                    topic_0, topic_1, topic_2 = self.extract_topics(
+                        topics=result["topics"]
+                    )
 
-                    data.append(MetadataModel(
-                        timestamp=timestamp, doi=result["doi"].replace("https://doi.org/", ""),
-                        cited_by_count=int(result["cited_by_count"]),
-                        open_access=result["open_access"]["is_oa"],
-                        topic_0=topic_0,
-                        topic_1=topic_1,
-                        topic_2=topic_2,
-                        json_data=result,
-                    ))
+                    data.append(
+                        MetadataModel(
+                            timestamp=timestamp,
+                            doi=result["doi"].replace("https://doi.org/", ""),
+                            cited_by_count=int(result["cited_by_count"]),
+                            open_access=result["open_access"]["is_oa"],
+                            topic_0=topic_0,
+                            topic_1=topic_1,
+                            topic_2=topic_2,
+                            json_data=result,
+                        )
+                    )
 
                 bar.next()
 
         return data
 
-    def execute(self)    ->  int:
+    def execute(self) -> int:
         # Get the current row count of the `openalex` table to ensure that the
         # SQL Unique constraint is not violated by updating DataFrame index
         # later
-        search_table_row_count: int = self.db.get_last_row_id(
-            table_name="openalex"
-        )
+        search_table_row_count: int = self.db.get_last_row_id(table_name="openalex")
 
         # Conduct searches
         self.logger.info(msg=f"Executing OpenAlex search")
         searches: list[MetadataModel] = self.search()
-        self.logger.info(
-            msg=f"Searched {len(searches)} documents"
-        )
+        self.logger.info(msg=f"Searched {len(searches)} documents")
 
         # Create DataFrame of searches
         self.logger.info(msg="Preparing searches for database write")
