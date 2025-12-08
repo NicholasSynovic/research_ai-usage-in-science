70d9c3d13e8e58aeae02c991448332713ad9cd24
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-12-08 12:37:14 -0600

    Fix pre-commit errors for runner entrypoint

--- aius/runners/__init__.py
+++ aius/runners/__init__.py
@@ -1,10 +1,11 @@
 """
-Runner entrypoints; each runner corresponds to a CLI sub-command.
+Entrypoint for runners.
 
 Copyright 2025 (C) Nicholas M. Synovic
 
 """
 
+from abc import ABC, abstractmethod
 from logging import Logger
 from pathlib import Path
 
@@ -17,98 +18,66 @@ from aius.runners.pandoc import PandocRunner
 from aius.runners.search import SearchRunner
 
 
+class Runner(ABC):  # noqa: D101
+    def __init__(self, name: str, db: DB) -> None:  # noqa: D107
+        self.name: str = name
+        self.db: DB = db
+
+    @abstractmethod
+    def execute(self) -> int: ...  # noqa: D102
+
+
 def connect_to_db(logger: Logger, db_path: Path) -> DB:  # noqa: D103
     logger.info("Connected to SQLite3 database: %s", db_path)
     return DB(logger=logger, db_path=db_path)
 
 
-def init(logger: Logger, **kwargs) -> None:  # noqa: ANN003, D103
-    logger.debug("init kwargs: %s", kwargs)
+def handle_runner(logger: Logger, runner_name: str, **kwargs) -> int:  # noqa: ANN003, D103
+    logger.info("%s kwargs: %s", runner_name, kwargs)
 
     # Connect to the database
-    db: DB = connect_to_db(logger=logger, db_path=kwargs["init.db"])
+    db: DB = connect_to_db(logger=logger, db_path=kwargs[f"{runner_name}.db"])
 
-    # Execute runner
-    runner: InitRunner = InitRunner(
-        db=db,
-        min_year=kwargs["init.min"],
-        max_year=kwargs["init.max"],
-        logger=logger,
-    )
-    runner.execute()
+    match runner_name:
+        case "init":
+            runner = InitRunner(
+                db=db,
+                min_year=kwargs["init.min"],
+                max_year=kwargs["init.max"],
+                logger=logger,
+            )
+        case "search":
+            runner = SearchRunner(
+                logger=logger,
+                db=db,
+                journal=kwargs["search.journal"],
+            )
+        case "openalex":
+            runner = OpenAlexRunner(
+                logger=logger,
+                db=db,
+                email=kwargs["openalex.email"],
+            )
+        case "jats":
+            runner = JATSRunner(
+                logger=logger,
+                db=db,
+                plos_zip_fp=kwargs["jats.plos_zip"],
+            )
+        case "pandoc":
+            runner = PandocRunner(
+                logger=logger,
+                db=db,
+                pandoc_uri=kwargs["pandoc.uri"],
+            )
+        case "analysis":
+            runner = AnalysisRunner(
+                logger=logger,
+                db=db,
+                prompt_id=kwargs["analysis.prompt"],
+                alcf_auth_token=kwargs["analysis.auth"],
+            )
+        case _:
+            return 1
 
-
-def search(logger: Logger, **kwargs) -> None:  # noqa: ANN003, D103
-    logger.debug("search kwargs: %s", kwargs)
-
-    # Connect to the database
-    db: DB = connect_to_db(logger=logger, db_path=kwargs["search.db"])
-
-    # Execute runner
-    runner: SearchRunner = SearchRunner(
-        logger=logger,
-        db=db,
-        journal=kwargs["search.journal"],
-    )
-    runner.execute()
-
-
-def openalex(logger: Logger, **kwargs) -> None:  # noqa: ANN003, D103
-    logger.debug("openalex kwargs: %s", kwargs)
-
-    # Connect to the database
-    db: DB = connect_to_db(logger=logger, db_path=kwargs["openalex.db"])
-
-    # Execute runner
-    runner: OpenAlexRunner = OpenAlexRunner(
-        logger=logger,
-        db=db,
-        email=kwargs["openalex.email"],
-    )
-    runner.execute()
-
-
-def jats(logger: Logger, **kwargs) -> None:  # noqa: ANN003, D103
-    logger.debug("jats kwargs: %s", kwargs)
-
-    # Connect to the database
-    db: DB = connect_to_db(logger=logger, db_path=kwargs["jats.db"])
-
-    # Execute runner
-    runner: JATSRunner = JATSRunner(
-        logger=logger,
-        db=db,
-        plos_zip_fp=kwargs["jats.plos_zip"],
-    )
-    runner.execute()
-
-
-def pandoc(logger: Logger, **kwargs) -> None:  # noqa: ANN003, D103
-    logger.debug("pandoc kwargs: %s", kwargs)
-
-    # Connect to the database
-    db: DB = connect_to_db(logger=logger, db_path=kwargs["pandoc.db"])
-
-    # Execute runner
-    runner: PandocRunner = PandocRunner(
-        logger=logger,
-        db=db,
-        pandoc_uri=kwargs["pandoc.uri"],
-    )
-    runner.execute()
-
-
-def analysis(logger: Logger, **kwargs) -> None:  # noqa: ANN003, D103
-    logger.debug("analysis kwargs: %s", kwargs)
-
-    # Connect to the database
-    db: DB = connect_to_db(logger=logger, db_path=kwargs["analysis.db"])
-
-    # Execute runner
-    runner: AnalysisRunner = AnalysisRunner(
-        logger=logger,
-        db=db,
-        prompt_id=kwargs["analysis.prompt"],
-        alcf_auth_token=kwargs["analysis.auth"],
-    )
-    runner.execute()
+    return runner.execute()
--- aius/runners/runner.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from logging import Logger
-
-
-class Runner(ABC):
-    @abstractmethod
-    def execute(self) -> int: ...
