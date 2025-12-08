0fc2b0c1f16c5fe1b55dc31d2e680e92a50973f1
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-12-08 12:08:08 -0600

    Fix pre-commit errors

--- aius/cli/__init__.py
+++ aius/cli/__init__.py
@@ -1,11 +1,17 @@
+"""
+CLI interface.
+
+Copyright (C) 2025 Nicholas M. Synovic
+"""
+
 from abc import ABC, abstractmethod
 from importlib.metadata import version
 
 DATABASE_HELP_MESSAGE: str = "Path to database"
 
 
-class CLI(ABC):
-    def __init__(self) -> None:
+class CLI(ABC):  # noqa: D101
+    def __init__(self) -> None:  # noqa: D107
         self.program_name: str = "AIUS"
         self.progam_epilog: str = "Copyright 2025 (C) Nicholas M. Synovic"
         self.program_description: str = """
@@ -16,37 +22,37 @@ in natural science publications
         self.construct_cli()
 
     @abstractmethod
-    def add_version(self) -> None: ...
+    def add_version(self) -> None: ...  # noqa: D102
 
     @abstractmethod
-    def add_init_subparser(self) -> None: ...
+    def add_init_subparser(self) -> None: ...  # noqa: D102
 
     @abstractmethod
-    def add_search_subparser(self) -> None: ...
+    def add_search_subparser(self) -> None: ...  # noqa: D102
 
     @abstractmethod
-    def add_openalex_subparser(self) -> None: ...
+    def add_openalex_subparser(self) -> None: ...  # noqa: D102
 
     @abstractmethod
-    def add_documents_subparser(self) -> None: ...
+    def add_documents_subparser(self) -> None: ...  # noqa: D102
 
     @abstractmethod
-    def add_pandoc_subparser(self) -> None: ...
+    def add_pandoc_subparser(self) -> None: ...  # noqa: D102
 
     @abstractmethod
-    def add_analyze_subparser(self) -> None: ...
+    def add_analyze_subparser(self) -> None: ...  # noqa: D102
 
     @abstractmethod
-    def parse_cli(self) -> dict: ...
+    def parse_cli(self) -> dict: ...  # noqa: D102
 
     @abstractmethod
-    def identify_subcommand(self) -> str: ...
+    def identify_subcommand(self) -> str: ...  # noqa: D102
 
     @property
-    def get_version(self) -> str:
+    def get_version(self) -> str:  # noqa: D102
         return version(distribution_name="aius")
 
-    def construct_cli(self) -> None:
+    def construct_cli(self) -> None:  # noqa: D102
         self.add_version()
         self.add_init_subparser()
         self.add_search_subparser()
